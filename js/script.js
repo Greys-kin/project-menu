979c5791cc99cdeef68fd1ba8dd7c1c03ab96951
greyskin <hissra6@gmail.com>
2024-11-10 18:16:09 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 16,
+    slidesPerView: 'auto',
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+
+const expandBtn = document.querySelector('.main__expand');
+const hideBtn = document.querySelector('.main__hide');
+expandBtn.addEventListener('click', btnExpand);
+hideBtn.addEventListener('click', btnHide);
+
+function btnExpand () {
+  const listItem = document.querySelectorAll('.list__item');
+  for (let i = 0; i < listItem.length; i++) {
+      listItem[i].classList.replace("hidden", 'visible');
+  }
+  expandBtn.style.display = "none";
+  hideBtn.style.display = 'block';
+}
+
+function btnHide () {
+  const listItem = document.querySelectorAll('.list__item');
+  for (let i = 0; i < listItem.length; i++) {
+      listItem[i].classList.replace("visible", 'hidden');
+  }
+  hideBtn.style.display = "none";
+  expandBtn.style.display = 'block';
+}
+
